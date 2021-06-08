abf1aafd62862104c1c92f803735ba14375b8cfc
Toshiki Hosojima <hosojimat@gmail.com>
2021-06-08 20:25:28 +0900

    csrf対策実装追記

--- www/model/functions.php
+++ www/model/functions.php
@@ -138,6 +138,7 @@ function is_valid_upload_image($image){
 function h($str){
   return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
 }
+
 // ランダムな文字列（トークン）を生成、セッションにトークンを設定し$tokenを返す
 function get_csrf_token(){
   $token = get_random_string(30);
